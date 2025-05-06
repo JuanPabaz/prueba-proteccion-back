0ab419f1462135bdfbab3653ae2d61553d1bc019
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-05-06 16:26:14 -0500

    feat: push ci/cd

--- src/main/java/com/prueba/proteccion/repositories/UserRepository.java
+++ src/main/java/com/prueba/proteccion/repositories/UserRepository.java
@@ -9,7 +9,7 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Integer> {
 
     Optional<User> findByUsername(String username);
-
+    //Try CI/CD
     @Query("SELECT u.role FROM User u WHERE u.username = :nombre")
     Role findRoleByUsername(String nombre);
 
